7d418ba9022ceaa63954d96514f2f1082edcf007
Sean King <r.sean.king@gmail.com>
2021-04-07 07:47:57 -0400

    Extract Fuction for User Agent String

--- src/js/window.js
+++ src/js/window.js
@@ -4,22 +4,7 @@ const windowStateKeeper = require('electron-window-state');
 const Badge = require('electron-windows-badge');
 
 const {isMacOS, isWindows} = require('./os-check');
-
-/* eslint max-len: ["error", { "ignoreStrings": true }]*/
-/**
- * Provides a Chrome user agent for the OS.
- *
- * @return {string} The user agent
- */
-function userAgent() {
-  if (isMacOS()) {
-    return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36';
-  } else if (isWindows()) {
-    return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36';
-  } else {
-    return 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36';
-  }
-}
+const { userAgent } = require('./user-agent');
 
 /**
  * Creates a new BrowserWindow.
